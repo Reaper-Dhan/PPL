b5da6c107c1116eb87b5b26487a5dfabf9ae64e2
Dhanvinesh K <75219613+Reaper-Dhan@users.noreply.github.com>
2023-05-09 12:19:23 +0530

    Update qs.hs

--- Haskell/qs.hs
+++ Haskell/qs.hs
@@ -1,5 +1,12 @@
 qs::[Int]->[Int]
-qs[] = []
-qs(x:xs) = qs smaller ++ [x] ++ qs larger
- where smaller = [a|a<-xs,a<=x]
-       larger = [b|b<-xs,b>x]
+qs[]=[]
+qs(x:xs)= 
+ let sm=[a|a<-xs,a<=x]      
+ bi=[a|a<-xs,a>x] in qs sm++[x]++qs bi 
+
+main::IO()
+main=do  
+putStrLn "Enter the element seperated by space"  
+input<-getLine  
+let nums= map read $ words input :: [Int]  
+putStrLn $ "sorted list:" ++ show (qs nums)
